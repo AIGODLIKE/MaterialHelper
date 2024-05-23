c4ee29700a65db67a4886e69a59ff8bfec686f30
1029920178@qq.com <1029910278@qq.com>
2024-05-23 22:58:37 +0800

    bump to 1.3.0, 4.1

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Material Helper 材质助手",
     "author": "AIGODLIKE社区, Atticus",
-    "version": (1, 2, 0),
-    "blender": (4, 0, 0),
+    "version": (1, 3, 0),
+    "blender": (4, 1, 0),
     "location": "资产管理器>顶部菜单>材质助手",
     "description": "将本地资产管理器作为你创造强大材质的地方吧",
     "warning": "",
