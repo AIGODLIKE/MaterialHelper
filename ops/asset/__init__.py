aaedc7edb2146ccd1d0681390c8fb5b71c28904c
小萌新 <3209970865@qq.com>
2025-07-14 18:14:46 +0800

    Feat: delete

--- ops/asset/__init__.py
+++ ops/asset/__init__.py
@@ -1,9 +1,11 @@
 import bpy
 
 from .duplicate import MATHP_OT_duplicate_asset
+from .delete import MATHP_OT_delete_asset
 
 class_list = [
     MATHP_OT_duplicate_asset,
+    MATHP_OT_delete_asset,
 ]
 
 register_class, unregister_class = bpy.utils.register_classes_factory(class_list)
--- ops/asset/delete.py
+++ ops/asset/delete.py
@@ -1,15 +1,21 @@
+import bpy
 
 
-class MATHP_OT_delete_asset(SelectedAsset, bpy.types.Operator):
+class MATHP_OT_delete_asset(bpy.types.Operator):
     """Delete Selected Materials"""
     bl_idname = "mathp.delete_asset"
     bl_label = "Delete"
     bl_options = {"UNDO"}
 
+    @classmethod
+    def poll(cls, context):
+        return hasattr(context, 'selected_assets') and context.selected_assets
+
     def invoke(self, context, event):
         return context.window_manager.invoke_confirm(self, event)
 
     def execute(self, context):
+        from ...utils import get_local_selected_assets
         match_obj = get_local_selected_assets(context)
         selected_mats = [obj for obj in match_obj if isinstance(obj, bpy.types.Material)]
 
@@ -17,5 +23,4 @@ class MATHP_OT_delete_asset(SelectedAsset, bpy.types.Operator):
             bpy.data.materials.remove(mat)
 
         bpy.ops.asset.library_refresh()
-
         return {"FINISHED"}
