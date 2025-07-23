2b2448bd8a82e07060509f1cdf0a0e3ba8225850
小萌新 <3209970865@qq.com>
2025-07-23 17:38:02 +0800

    Feat: modify code

--- preferences/sync.py
+++ preferences/sync.py
@@ -1,11 +1,16 @@
 import bpy
+from ..sync.material_to_asset import AssetSync
 
 
 class Sync:
+    def update_material(self, context):
+        AssetSync.sync()
+
     auto_update: bpy.props.BoolProperty(
         name='Auto Update',
         default=True,
         description='If checked, the material will be automatically add as temp asset\nElse, temp assets will be cleared',
+        update=update_material,
     )
 
     sync_select: bpy.props.BoolProperty(
--- sync/material_to_asset.py
+++ sync/material_to_asset.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 import bpy
 
+from ..debug import DEBUG_ENSURE_CURRENT_FILE_ASSET_CATS
 from ..utils import MATERIAL_HELPER_ASSET_UUID, MATERIAL_HELPER_ASSET_TAG, get_pref, tag_redraw
 
 
 def cat_uuid_in_file(path: Path):
-    with open(path, 'r', encoding='utf-8') as f:
+    with open(path, "r", encoding="utf-8") as f:
         for i, line in enumerate(f.readlines()):
             if ":" not in line: continue
             uuid = line.split(":")[0]
@@ -18,14 +19,24 @@ def cat_uuid_in_file(path: Path):
 
 def append_asset_cats_txt(path: Path) -> None:
     try:
-        with open(path, 'a', encoding='utf-8') as f:
+        with open(path, "a", encoding="utf-8") as f:
             f.write(f"{MATERIAL_HELPER_ASSET_UUID}:Material Helper:Material Helper\n")
     except PermissionError:
-        print('Material Helper: Permission Denied')
+        print("Material Helper: Permission Denied")
     except FileNotFoundError:
-        print('Material Helper: Category file not found')
+        print("Material Helper: Category file not found")
     except Exception as e:
-        print('Unexpected Error:', e)
+        print("Unexpected Error:", e)
+    else:
+        print("Material Helper: Category file updated")
+
+
+def select_catalog():
+    for win in bpy.data.window_managers[0].windows:
+        for area in win.screen.areas:
+            for space in area.spaces:
+                if space.type == "ASSET":
+                    space.params.catalog_id = MATERIAL_HELPER_ASSET_UUID
 
 
 class AssetSync:
@@ -40,6 +51,7 @@ class AssetSync:
         if pref.auto_update:
             if not cls.is_sync:
                 cls.is_sync = True
+                select_catalog()
             if len(bpy.data.materials) != cls.material_count:
                 cls.material_sync_asset()
         elif cls.is_sync:  # 关闭同步
@@ -64,8 +76,8 @@ class AssetSync:
             mat.asset_mark()
             mat.asset_generate_preview()
             mat.asset_data.tags.new(MATERIAL_HELPER_ASSET_TAG)
-            if mat.asset_data.catalog_id != MATERIAL_HELPER_ASSET_UUID:
-                mat.asset_data.catalog_id = MATERIAL_HELPER_ASSET_UUID
+            mat.asset_data.catalog_id = MATERIAL_HELPER_ASSET_UUID
+
         tag_redraw()
 
     @classmethod
@@ -74,23 +86,28 @@ class AssetSync:
 
     @classmethod
     def ensure_current_file_asset_cats(cls):
-        if bpy.data.filepath == '': # 未保存文件无法写入
-            print("Material Helper: File Not Save! Set category failed")
+        if bpy.data.filepath == "":  # 未保存文件无法写入
+            if DEBUG_ENSURE_CURRENT_FILE_ASSET_CATS:
+                print("Material Helper: File Not Save! Set category failed")
             return
 
-        cat_path = Path(bpy.data.filepath).parent.joinpath('blender_assets.cats.txt')
-        cat_path_mod = Path(bpy.data.filepath).parent.joinpath('blender_assets.cats.txt~')
+        cat_path = Path(bpy.data.filepath).parent.joinpath("blender_assets.cats.txt")
+        cat_path_mod = Path(bpy.data.filepath).parent.joinpath("blender_assets.cats.txt~")
 
         if cat_path_mod.exists():  # delete
             cat_path_mod.unlink()
 
         if cat_path.exists():
+            if DEBUG_ENSURE_CURRENT_FILE_ASSET_CATS:
+                print("cat_uuid_in_file", cat_uuid_in_file(cat_path))
             if not cat_uuid_in_file(cat_path):
-                print('Material Helper: Writing category to current file')
+                if DEBUG_ENSURE_CURRENT_FILE_ASSET_CATS:
+                    print("Material Helper: Writing category to current file")
                 append_asset_cats_txt(cat_path)
         else:
-            with open(cat_path, "w+", encoding='utf-8') as f:
-                print('Material Helper Creating Category')
+            with open(cat_path, "w+", encoding="utf-8") as f:
+                if DEBUG_ENSURE_CURRENT_FILE_ASSET_CATS:
+                    print("Material Helper Creating Category")
                 f.write(f"""# This is an Asset Catalog Definition file for Blender.
     #
     # Empty lines and lines starting with `#` will be ignored.
