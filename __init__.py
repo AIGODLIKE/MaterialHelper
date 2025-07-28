d724a51267e4543d8d006392fb3acf22c58e70db
小萌新 <3209970865@qq.com>
2025-07-28 11:02:44 +0800

    Feat: add author

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "Material Helper 材质助手",
-    "author": "AIGODLIKE社区, Atticus",
+    "author": "AIGODLIKE社区, Atticus, 小萌新",
     "version": (1, 4, 1),
     "blender": (4, 5, 0),
     "location": "资产管理器>顶部菜单>材质助手",
