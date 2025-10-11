f940e85bd6e3ea1d4a341dc463809108d7314413
小萌新 working <3209970865@qq.com>
2025-10-11 12:06:26 +0800

    Feat: optimize mathp.select_material_obj bpy.ops

--- ops/select_material.py
+++ ops/select_material.py
@@ -8,23 +8,15 @@ class MATHP_OT_select_material_obj(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return hasattr(context, 'selected_assets') and context.selected_assets
+        return hasattr(context, 'selected_assets') and context.selected_assets and context.mode == "OBJECT"
 
     def execute(self, context):
         from ..utils import get_local_selected_assets
         match_obj = get_local_selected_assets(context)
         selected_mats = [obj for obj in match_obj if isinstance(obj, bpy.types.Material)]
-        tmp_mesh = bpy.data.meshes.new("Temp")
-        tmp_obj = bpy.data.objects.new("Temp", tmp_mesh)
-        context.collection.objects.link(tmp_obj)
-        bpy.context.view_layer.objects.active = tmp_obj
-
-        bpy.ops.object.mode_set(mode="OBJECT")
-        bpy.ops.object.select_all(action="DESELECT")
-        temp_obj = context.object
-        for i, mat in enumerate(selected_mats):
-            bpy.ops.object.material_slot_add()
-            tmp_obj.material_slots[i].material = mat
-        bpy.ops.object.select_linked(type="MATERIAL")
-        bpy.data.objects.remove(temp_obj)
+        for obj in context.scene.objects:
+            if materials := getattr(obj.data, "materials", None):
+                for mat in materials:
+                    if mat in selected_mats:
+                        obj.select_set(True)
         return {"FINISHED"}
